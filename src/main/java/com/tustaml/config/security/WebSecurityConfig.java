9a9acccfc8ae392c5b48bdf9589bb82ae3a4095b
shree krishna <shreekrishna905@gmail.com>
2017-07-01 22:38:42 +0545

    oAuth Configuration

--- src/main/java/com/tustaml/config/security/WebSecurityConfig.java
+++ src/main/java/com/tustaml/config/security/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -38,15 +37,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().antMatchers("/oauth/token").permitAll().and().formLogin()
-				.loginPage("/").permitAll();
+		http.authorizeRequests().anyRequest().authenticated().antMatchers("/oauth/token").permitAll();
 	}
 
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/webjars/**");
-		web.ignoring().antMatchers("/static/**");
-	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
